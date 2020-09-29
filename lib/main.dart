41e5076185253104cbd9afee93b75393296c4fed
Alba Torres Rodriguez <alba@jointhebase.co>
2020-09-29 12:30:22 +0200

    if I comment the code if (frozen)return; it falls but ontap not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math' as math;
+
+
 import 'package:flame/components/animation_component.dart';
 import 'package:flame/components/mixins/resizable.dart';
 import 'package:flame/flame.dart';
@@ -8,7 +11,7 @@ import 'package:flame/anchor.dart';
 import 'package:flame/components/component.dart';
 
 
-const COLOR = const Color(0xFFDDC0A3);
+const COLOR = const Color(0xFF75DA8B);
 const SIZE = 52.0;
 const GRAVITY = 400.0;
 const BOOST = -380.0;
@@ -16,12 +19,12 @@ const BOOST = -380.0;
 void main() async{
   WidgetsFlutterBinding.ensureInitialized ();
   final size = await Flame.util.initialDimensions();
-  final game = MyApp(size);
+  final game = MyGame(size);
   runApp(game.widget);
 }
 
 // Resizable will resize the with and height of the background component depending on the size of the mobile
-class Background extends Component with Resizable{
+class Bg extends Component with Resizable{
   static final Paint _paint = Paint()..color = COLOR;
   @override
   void render(Canvas c) {
@@ -31,73 +34,73 @@ class Background extends Component with Resizable{
   @override
   void update(double t) {
   }
-  
+
 }
 
 
 class Bird extends AnimationComponent with Resizable{
   // to move the bird so it "falls" we move the speed to the Y direction
-  double speedY = 0.0; 
+  double speedY = 0.0;
   bool frozen;
   // The amount is the number of frames, so i this case 4
-  Bird () : super.sequenced(SIZE, SIZE, 'assets/images/bird.png', 4, textureWidth: 16.0, textureHeight: 16.0) {
-    this.anchor = Anchor.center; 
+  Bird () : super.sequenced(SIZE, SIZE, 'bird.png', 4, textureWidth: 16.0, textureHeight: 16.0) {
+    this.anchor = Anchor.center;
   }
 
   // angle so the bird looks up or down
-  Position get velocity => Position (300.0, speedY); 
+  Position get velocity => Position (300.0, speedY);
 
 
   reset () {
-    this.x = size.width/ 2; 
-    this.y = size.height/2; 
-    speedY = 0; 
-    frozen = true; 
-    angle = 0.0; 
+    this.x = size.width/ 2;
+    this.y = size.height/2;
+    speedY = 0;
+    frozen = true;
+    angle = 0.0;
   }
 
   @override
   void resize(Size size) {
-    // everytime updates resizes 
-    super.resize(size); 
+    // everytime updates resizes
+    super.resize(size);
     reset();
   }
 
   @override
   void update(double t) {
     // TODO: implement update // we update the Y and the speed
-    // formula of gravity to make it seem like its real 
+    // formula of gravity to make it seem like its real
     super.update(t);
     // when its frozen we just return and do nothing. We need onTap for the bird to move
-    if (frozen) return; 
-    this.y += speedY * t - GRAVITY * t * t / 2; 
-    this.speedY += GRAVITY *t; 
-    this.angle = velocity.angle(); 
-
+    if (frozen) return;
+    this.y += speedY * t - GRAVITY * t * t / 2;
+    this.speedY += GRAVITY * t;
+    this.angle = velocity.angle();
+    // if birds falls out of the screen reset()
     if (y > size.height) {
-      reset(); 
+      reset();
     }
   }
   onTap () {
     if (frozen) {
-      frozen = false; 
-      return; 
+      frozen = false;
+      return;
     }
-    speedY = (speedY + BOOST).clamp(BOOST, speedY); 
+    speedY = (speedY + BOOST).clamp(BOOST, speedY);
   }
-  
+
 }
 
 
-class MyApp extends BaseGame {
+class MyGame extends BaseGame {
 
-  Bird bird; 
-  MyApp (Size size){
-    add(Background());
+  Bird bird;
+  MyGame (Size size){
+    add(Bg());
     add(bird = Bird());
-
   }
+  @override
   void onTap() {
-    bird.onTap(); 
+    bird.onTap();
   }
 }
